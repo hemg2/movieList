e2b6643ab00c07a66dd9c893fd471056963f3abd
hemg2 <hemg2@naver.com>
2022-12-26 23:27:18 +0900

    Content

---
+++ Model/Content.swift
@@ -0,0 +1,8 @@
+//
+//  Content.swift
+//  MovieListSample
+//
+//  Created by 1 on 2022/12/26.
+//
+
+import UIKit
--- View/HomeViewController.swift
+++ View/HomeViewController.swift
@@ -15,5 +15,12 @@ class HomeViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationController?.navigationBar.backgroundColor = .clear
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.hidesBarsOnSwipe = true
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "netflix_icon"), style: .plain, target: nil, action: nil)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "person.crop.circle"), style: .plain, target: nil, action: nil)
     }
 }
